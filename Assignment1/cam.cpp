57385b9f15b7128c08161ad22afce4fd6baaa28f
its-sachin <sachinsain28786@gmail.com>
2021-02-22 22:16:29 +0530

    points selection done(error in transparent image)

--- Assignment1/cam.cpp
+++ Assignment1/cam.cpp
@@ -5,15 +5,22 @@ using namespace cv;
 using namespace std;
 
 // function to be given to set mouse callback (PROBLEM HERE)
-void selectingPoints(int event, int x, int y, int flag, void* points) {
-    vector<Point2f> inPoints = *((vector<Point2f> *) points);
+void selectingPoints(int event, int x, int y, int flag, void* image) {
+
+    Mat sourcImg = *reinterpret_cast<Mat*>(image);
+
+    vector<Point2f> inPoints;
+
     if (event == EVENT_LBUTTONDOWN) {
+        cout << x << endl;
         if (inPoints.size() < 4) {
             inPoints.push_back(Point2f(x,y));
-            cout << x << endl;
         }
 
     }
+
+    image = reinterpret_cast<void*>(&inPoints);
+
 }
 
 
@@ -21,6 +28,7 @@ int main(int argc, char* argv[]) {
 
     // reading the image (grayscale) 
     Mat sourcImg = imread("empty.jpg",0);
+    Mat wrapImg = Mat::zeros(sourcImg.size(),CV_8UC3);
 
     if (sourcImg.empty()) {
         cout << "Unable to open the source image file - Check if empty.jpg present in the folder or not." << endl;
@@ -30,7 +38,6 @@ int main(int argc, char* argv[]) {
       
     // display grayscaled source image
     imshow("Original Frame", sourcImg);
-    waitKey(0); // ---> to be changed.
 
     // maping points given on course website
     vector<Point2f> mapPoints;
@@ -39,20 +46,20 @@ int main(int argc, char* argv[]) {
     mapPoints.push_back(Point2f(800,830));
     mapPoints.push_back(Point2f(800,52));
 
-    // points to be selected by user
-    vector<Point2f> inPoints;
-    setMouseCallback("Original Image", selectingPoints, &inPoints);
+    void* data = reinterpret_cast<void*>(&sourcImg);
+    
+    setMouseCallback("Original Frame", selectingPoints, data);
+    waitKey(0); // ---> to be changed.
+
+    vector<Point2f> inPoints = *reinterpret_cast<Mat*>(data);
 
     // do homography
-    Mat wrapedMatrix = findHomography(inPoints, mapPoints);
-    Mat wrapImg = Mat::zeros(sourcImg.size(),CV_8UC3);
+    Mat wrapedMatrix = findHomography(mapPoints, mapPoints);
     warpPerspective(sourcImg, wrapImg, wrapedMatrix, sourcImg.size());
 
     imshow("Image", wrapImg);
     waitKey(0);
-
-
-    // selecting the points 
+ 
     
 }
 
