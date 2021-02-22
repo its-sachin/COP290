6bf2720ca13d970b33cef9874c867a699e2c5e72
its-sachin <sachinsain28786@gmail.com>
2021-02-22 19:37:37 +0530

    dono homography (error in points selection)

--- Assignment1/cam.cpp
+++ Assignment1/cam.cpp
@@ -4,6 +4,18 @@
 using namespace cv;
 using namespace std;
 
+// function to be given to set mouse callback (PROBLEM HERE)
+void selectingPoints(int event, int x, int y, int flag, void* points) {
+    vector<Point2f> inPoints = *((vector<Point2f> *) points);
+    if (event == EVENT_LBUTTONDOWN) {
+        if (inPoints.size() < 4) {
+            inPoints.push_back(Point2f(x,y));
+            cout << x << endl;
+        }
+
+    }
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -19,5 +31,29 @@ int main(int argc, char* argv[]) {
     // display grayscaled source image
     imshow("Original Frame", sourcImg);
     waitKey(0); // ---> to be changed.
+
+    // maping points given on course website
+    vector<Point2f> mapPoints;
+    mapPoints.push_back(Point2f(472,52));
+    mapPoints.push_back(Point2f(472,830));
+    mapPoints.push_back(Point2f(800,830));
+    mapPoints.push_back(Point2f(800,52));
+
+    // points to be selected by user
+    vector<Point2f> inPoints;
+    setMouseCallback("Original Image", selectingPoints, &inPoints);
+
+    // do homography
+    Mat wrapedMatrix = findHomography(inPoints, mapPoints);
+    Mat wrapImg = Mat::zeros(sourcImg.size(),CV_8UC3);
+    warpPerspective(sourcImg, wrapImg, wrapedMatrix, sourcImg.size());
+
+    imshow("Image", wrapImg);
+    waitKey(0);
+
+
+    // selecting the points 
+    
 }
+
 
