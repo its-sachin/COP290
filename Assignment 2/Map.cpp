ed67ec2bcb8868c2ed236886d19fea8ee1b784d1
heisenberg600 <saurabhverma.iitd@gmail.com>
2021-04-19 20:01:27 +0530

    generate map function

--- Assignment 2/Map.cpp
+++ Assignment 2/Map.cpp
@@ -7,6 +7,7 @@ class Map{
 private:
     int height;
     int width;
+    string path;
     vector<vector<Tile*>> tileStruct;
 
     int playerPos[2];
@@ -17,10 +18,16 @@ private:
 
 public:
 
-    Map(string path) {
+    Map(string pathM) {
+        path=pathM;
+    }
+
+    bool genrateMap(){
         fstream file;
         file.open(path.c_str(), std::ios::in);
-
+        if (file.is_open() == false){
+            return false;
+        }
         int count = 0;
         string line1;
     
@@ -78,23 +85,15 @@ public:
                         tile= new Tile(x,y,false,false);
                         break;
                 }
-                trow.push_back(tile);
+                if (tile!=NULL){
+                    trow.push_back(tile);
+                }
             }
             tileStruct.insert(tileStruct.begin(),trow);
             y--;
         }
         width=tileStruct[0].size();
         file.close();
-            //initialise
-
-        //here read the text file provided and make tiles, sstore them in tileStruct
-        // if "." then make isCoin True,
-        // if "x" then make isBrick True;
-        // if "p" then set playerpos[2] to (i,j)
-        // if "e1" then set blinkypos[2] to (i,j)
-        // same for other enemies
-        // when we will create the instance of map class in oure pacman class we will get these initial position and 
-        // use then to inittialise our player and enemy
     }
 
     int getHeight() {return height;}
