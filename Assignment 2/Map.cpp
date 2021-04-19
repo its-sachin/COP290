589224d0b7f498e755891faecfac0dbc47a6a488
heisenberg600 <saurabhverma.iitd@gmail.com>
2021-04-19 19:42:29 +0530

    error in map removed

--- Assignment 2/Map.cpp
+++ Assignment 2/Map.cpp
@@ -1,12 +1,13 @@
 #include "Tile.cpp"
 #include <vector>
+#include<fstream>
 
 class Map{
 
 private:
     int height;
     int width;
-    vector<vector<Tile*>>* tileStruct;
+    vector<vector<Tile*>> tileStruct;
 
     int playerPos[2];
     int blinkyPos[2];
@@ -17,83 +18,73 @@ private:
 public:
 
     Map(string path) {
-        std::fstream file;
+        fstream file;
         file.open(path.c_str(), std::ios::in);
 
-        if (file.is_open() == false)
-            return false;
-
         int count = 0;
-        std::string line1;
+        string line1;
     
-        while (getline(file, line)){
+        while (getline(file, line1)){
            count++;
         }
         height=count;
         file.open(path.c_str(), std::ios::in);
-        std::string line;
+        string line;
 
         int x=0;
         int y=count-1;
 
-        vector<vector<Tile*>> tStruct;
-
         while (std::getline(file, line)) {
             vector<Tile*> trow;
 
-            std::vector<char> chars(line.begin(), line.end());
+            vector<char> chars(line.begin(), line.end());
 
-            for (unsigned int x = 0; x < chars.size(); x++) {
-                
-                Tile tile;
-
+            for (int x = 0; x < chars.size(); x++) {
+                Tile* tile=NULL;
                 switch (chars[x])
                 {
                     case 'x':
-                        tile= Tile(x,y,true,false);
+                        tile= new Tile(x,y,true,false);
                         break;
                     case '.':
-                        tile= Tile(x,y,false,true);
+                        tile= new Tile(x,y,false,true);
                         break;
                     case 'p':
-                        tile= Tile(x,y,false,false);
+                        tile=new Tile(x,y,false,false);
                         playerPos[0]=x;
                         playerPos[1]=y;
                         break;
                     case 'b':
-                        tile= Tile(x,y,false,false);
+                        tile=new Tile(x,y,false,false);
                         blinkyPos[0]=x;
                         blinkyPos[1]=y;
                         break;
                     case 'i':
-                        tile= Tile(x,y,false,false);
+                        tile=new Tile(x,y,false,false);
                         inkyPos[0]=x;
                         inkyPos[1]=y;
                         break;
                     case 'n':
-                        tile= Tile(x,y,false,false);
+                        tile= new Tile(x,y,false,false);
                         pinkyPos[0]=x;
                         pinkyPos[1]=y;
                         break;
                     case 'c':
-                        tile= Tile(x,y,false,false);
+                        tile= new Tile(x,y,false,false);
                         clydePos[0]=x;
                         clydePos[1]=y;
                         break;
                     case ' ':
-                        tile= Tile(x,y,false,false);
+                        tile= new Tile(x,y,false,false);
                         break;
                 }
-
-                if (tile != NULL)
-                    trow.push_back(tile&);
+                trow.push_back(tile);
             }
-            tStruct.insert(tStruct.begin(),trow);
+            tileStruct.insert(tileStruct.begin(),trow);
             y--;
         }
-        width=tStruct[0].size();
+        width=tileStruct[0].size();
         file.close();
-        tileStruct=tStruct&;
             //initialise
 
         //here read the text file provided and make tiles, sstore them in tileStruct
