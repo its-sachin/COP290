455aa90c627f783e22bfe2c0f8b34eadea5a8511
heisenberg600 <saurabhverma.iitd@gmail.com>
2021-04-25 14:28:14 +0530

    classes for socket created

---
+++ Assignment 2/Client.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
+
+using namespace std;
+
+class Client{
+    private:
+
+    char* serverIp="127.1.0.0";
+    int portNum=8080;
+    char message[1024];
+    struct hostent* host;
+    sockaddr_in sendAddr;
+    socklen_t sendAddrSize;
+    int clientSd,status;
+    public: 
+
+    ~Client(){
+        close(clientSd);
+    }
+    void setIp(string s){
+        serverIp=const_cast<char*>(s.c_str());
+    }
+    void setPort(int p){
+        portNum=p;
+    }
+    void setMessage(string s){
+        memset(&message, 0, sizeof(message));
+        strcpy(message,s.c_str());
+    }
+    void innit(){
+        host=gethostbyname(serverIp);
+        sendAddrSize=sizeof(sendAddr);
+        bzero((char*)&sendAddr,sendAddrSize);
+        sendAddr.sin_family=AF_INET;
+        sendAddr.sin_addr.s_addr=inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+        sendAddr.sin_port = htons(portNum);
+        clientSd = socket(AF_INET, SOCK_STREAM, 0);
+        status=connect(clientSd,(sockaddr*) &sendAddr, sendAddrSize);
+        if (status<0){
+            cerr<<"Error connecting to socket!"<<endl; return ;
+        }
+    }
+
+    void sendMessage(string s){
+        setMessage(s);
+        send(clientSd,(char*)&message,strlen(message),0);
+    }
+
+    void recieveMessage(){
+        memset(&message,0,sizeof(message));
+        recv(clientSd,(char*)&message,sizeof(message),0);
+    }
+
+
+};
---
+++ Assignment 2/Server.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
+
+using namespace std;
+
+class Server{
+    private:
+    int portNum=8080;
+    char message[1024];
+    sockaddr_in servAddr;
+    sockaddr_in clientAddr;
+    socklen_t clientAddrSize;
+    socklen_t servAddrSize;
+    int serverSd,clientSd;
+    int bindStatus=-1;
+    public:
+
+    ~Server(){
+        close(clientSd);
+        close(serverSd);
+    }
+
+    void setPort(int p){
+        portNum=p;
+    }
+    void setMessage(string s){
+        memset(&message, 0, sizeof(message));
+        strcpy(message,s.c_str());
+    }
+    void innit(){
+        servAddrSize=sizeof(servAddr);
+        bzero((char*)&servAddr, servAddrSize);
+        servAddr.sin_family = AF_INET;
+        servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        servAddr.sin_port = htons(portNum);    
+        serverSd = socket(AF_INET, SOCK_STREAM, 0);   
+        if(serverSd < 0){
+            cerr << "Error establishing the server socket" << endl;
+            return;
+        } 
+        bindStatus = bind(serverSd, (struct sockaddr*) &servAddr,sizeof(servAddr));
+        if(bindStatus < 0){
+            cerr << "Error binding socket to local address" << endl;
+            return;
+        }  
+        listen(serverSd, 1);
+        clientAddrSize=sizeof(clientAddr);
+        clientSd = accept(serverSd, (sockaddr *)&clientAddr, &clientAddrSize);   
+        if (clientSd < 0){
+            cerr << "Error accepting request from client!" << endl;
+            return;
+        }    
+    }
+    void sendMessage(string s){
+        setMessage(s);
+        send(clientSd, (char*)&message,strlen(message),0);
+    }
+    void recieveMessage(){
+        memset(&message,0,sizeof(message));
+        recv(clientSd,(char*)&message,sizeof(message),0);
+    }
+};
